cb84d291467c26192dc30c588bc0b7431318811f
julianrcook <julianrcook@gmail.com>
2016-09-07 22:09:09 -0400

    Contains assignment 1 for r
    But uses R 3.2.1 not 3.1.x

---
+++ assignment1.r
@@ -0,0 +1,34 @@
+R version 3.2.1 (2015-06-18) -- "World-Famous Astronaut"
+Copyright (C) 2015 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+# snip
+
+> 2015^(1/3)
+[1] 12.63063
+> abs(5.7-6.8)/.58
+[1] 1.896552
+> a <- 1:12
+> a
+ [1]  1  2  3  4  5  6  7  8  9 10 11 12
+> b <- c(1, 3, 5, 7, 9, 11)
+> b
+[1]  1  3  5  7  9 11
+> c <- seq(1,11, 2)
+> c
+[1]  1  3  5  7  9 11
+> ln.a <- log(a)
+> ln.a
+ [1] 0.0000000 0.6931472 1.0986123 1.3862944 1.6094379 1.7917595 1.9459101 2.0794415
+ [9] 2.1972246 2.3025851 2.3978953 2.4849066
+> c^2
+[1]   1   9  25  49  81 121
+> # Q1.7 (above) Computes the squares of the odd numbers from 1 to 11.
+> ?sd
+> # the sd'  function computes the standard deviation of the values in x
+> # usage is: sd(x, na.rm = FALSE)
+> # Q1.9. Create a variable Name that contains your first name
+> Name <- "Julian"
+> paste("My name is", Name)
+[1] "My name is Julian"
+> 
